827392e629d4aee09d51f2796b7b45a62e3dce3f
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-19 01:07:42 +0200

    Add test for empty Grid

--- gamerino/src/main.rs
+++ gamerino/src/main.rs
@@ -30,6 +30,27 @@ struct Grid {
     squares: Vec<Square>
 }
 
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_empty_grid() {
+        let grid = ::Grid::generate_empty(5, 13);
+        assert_eq!(grid.size, (5, 13));
+        let mut number_of_squares = 0;
+
+        for square in &grid.squares {
+            assert_eq!(square.ground, ::TerrainGround::Soil);
+            assert_eq!(square.block, None);
+            assert_eq!(square.being, None);
+            number_of_squares += 1;
+        }
+
+        assert_eq!(grid.squares.len(), 5*13);
+        assert_eq!(number_of_squares, 5*13);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
