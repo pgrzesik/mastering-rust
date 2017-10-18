853c1ae340f83671b891b2715a170fabdefa4aa2
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-19 01:02:47 +0200

    Add enum and structs that will be used in gamerino

--- gamerino/src/main.rs
+++ gamerino/src/main.rs
@@ -1,3 +1,35 @@
+#[derive(PartialEq, Debug)]
+enum TerrainGround {
+    Soil,
+    Stone
+}
+
+#[derive(PartialEq, Debug)]
+enum TerrainBlock {
+    Tree,
+    Soil,
+    Stone
+}
+
+#[derive(PartialEq, Debug)]
+enum Being {
+    Orc,
+    Human
+}
+
+
+struct Square {
+    ground: TerrainGround,
+    block: Option<TerrainBlock>,
+    being: Option<Being>
+}
+
+
+struct Grid {
+    size: (usize, usize),
+    squares: Vec<Square>
+}
+
 fn main() {
     println!("Hello, world!");
 }
