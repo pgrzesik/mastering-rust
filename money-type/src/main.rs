e9a7dc1fd77f9a9d723f826ae2f6dce3438f78c4
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-20 19:03:10 +0200

    Implement Add for Money

--- money-type/src/main.rs
+++ money-type/src/main.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
 enum Currency {
     EUR,
     USD,
@@ -12,12 +14,32 @@ struct Money {
     currency: Currency
 }
 
+impl Add for Money {
+    type Output = Money;
+
+    fn add(self, other: Money) -> Money {
+        assert_eq!(self.currency, other.currency, "Trying to add different currencies!");
+
+        Money {
+            amount: self.amount + other.amount,
+            currency: self.currency
+        }
+    }
+}
+
 
 fn main() {
-    let euro = Money {
+    let hundred_euro = Money {
         amount: 100,
         currency: Currency::EUR
     };
 
-    println!("Euro money struct: {:?}", euro);
+    let five_hundred_euro = Money {
+        amount: 500,
+        currency: Currency::EUR
+    };
+
+    let six_hundred_euro = hundred_euro + five_hundred_euro;
+
+    println!("Result of adding two EUR money: {:?}", six_hundred_euro);
 }
