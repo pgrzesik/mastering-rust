25cd6bc0fd7b0206280fb4dad519715ec4b4641b
Piotr Grzesik <pj.grzesik@gmail.com>
2017-10-20 18:57:54 +0200

    Implement money and currency struct and enum

--- money-type/src/main.rs
+++ money-type/src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug)]
+enum Currency {
+    EUR,
+    USD,
+    PLN,
+    CAD
+}
+
+#[derive(Debug)]
+struct Money {
+    amount: u32,
+    currency: Currency
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let euro = Money {
+        amount: 100,
+        currency: Currency::EUR
+    };
+
+    println!("Euro money struct: {:?}", euro);
 }
